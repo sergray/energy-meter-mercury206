c8612402cd845d26d10b66f1ae514bebba2261bd
Sergey Panfilov <sergray@gmail.com>
2014-03-26 14:30:57 +0400

    Fix names collisions

--- mercury206/scripts.py
+++ mercury206/scripts.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 import config
 from communications import open_serial
-from commands import display_readings, instant_vcp
+import commands
 
 
 
@@ -21,7 +21,7 @@ def display_readings():
     "Display meter readings"
     settings = config.get_settings()
     port = open_serial(settings['device'])
-    readings = display_readings(port, settings['address'])
+    readings = commands.display_readings(port, settings['address'])
     print("{} kWh;{} kWh;{} kWh".format(*readings))
     return 0
 
@@ -30,6 +30,6 @@ def instant_vcp():
     "Display instant voltage, current and power consumption"
     settings = config.get_settings()
     port = open_serial(settings['device'])
-    voltage, current, power = instant_vcp(port, settings['address'])
+    voltage, current, power = commands.instant_vcp(port, settings['address'])
     print("{0} V;{1} A;{2} kW".format(voltage, current, power))
     return 0
